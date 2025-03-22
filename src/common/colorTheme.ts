c1c7815ca620f430e62f5084e83725dec60dabe9
olegus199 <okremnyov@yandex.ru>
2025-03-22 20:19:30 +0300

    feat: update global styles

---
+++ src/common/colorTheme.ts
@@ -0,0 +1,13 @@
+export const colorTheme = {
+  primary: {
+    main: "#3877ee",
+    dark: "#1e66eb",
+  },
+  secondary: {
+    main: "#ac3931",
+    contrastText: "#ac3931",
+  },
+  info: {
+    main: "#ccc",
+  },
+};
