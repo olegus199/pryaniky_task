12c8597974dffc2cff378590e742aeddc50c0bc6
olegus199 <okremnyov@yandex.ru>
2025-03-23 06:46:54 +0300

    feat: add Protected route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,63 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import DocsList from "../features/docs/DocsList.tsx";
-import { useEffect } from "react";
+import { FC, ReactNode } from "react";
+import AddEditDocForm from "../features/docs/AddEditDocForm.tsx";
+import { useAppDispatch } from "./hooks-redux.ts";
+import { tokenChanged } from "../features/auth/authTokenSlice.ts";
+import Login from "../features/auth/Login.tsx";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
+  const token = localStorage.getItem("authToken");
+  const dispatch = useAppDispatch();
+
+  if (!token) {
+    return <Navigate
+      to="/"
+      replace
+    />;
+  } else {
+    dispatch(tokenChanged(token));
+  }
+
+  return children;
+};
 
 function App() {
-  useEffect(() => {
-    async function testApi() {
-      const result = await fetch(`https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/login`, {
-        method: "POST",
-        body: JSON.stringify({ username: "user1", password: "password" }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await result.json();
-      console.log(data);
-    }
-
-    // testApi();
-  }, []);
   return (
-    <>
-      <DocsList />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Login />}
+        />
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <Routes>
+                <Route
+                  path={"/docs"}
+                  element={<DocsList />}
+                />
+                <Route
+                  path="/add-edit-doc"
+                  element={<AddEditDocForm />}
+                />
+                <Route
+                  path="/add-edit-doc/:id"
+                  element={<AddEditDocForm />}
+                />
+              </Routes>
+            </ProtectedRoute>
+          }
+        >
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
